da8093786246313493d6598cf54e319951a12e5b
RebeccaNiem <161700704+RebeccaNiem@users.noreply.github.com>
2024-09-19 19:05:24 +0200

    added Api integration

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="Moscow" />
         <footer>
           This project was built by Rebecca and is hosted on{" "}
           <a
--- src/Weather.js
+++ src/Weather.js
@@ -2,17 +2,25 @@ import React, { useState } from "react";
 import "./Weather.css";
 import axios from "axios";
 
-export default function Weather() {
-  const [ready, setReady] = useState(false);
-  const [temperature, setTemperature] = useState(null);
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
 
   function handleResponse(response) {
     console.log(response.data);
-    setTemperature(response.data.main.temp);
-    setReady(true);
+
+    setWeatherData({
+      ready: true,
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      city: response.data.name,
+      date: "Sunday, 10am",
+      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+      description: response.data.weather[0].description,
+    });
   }
 
-  if (ready) {
+  if (weatherData.ready) {
     return (
       <div className="Weather">
         <form>
@@ -39,28 +47,28 @@ export default function Weather() {
         <p></p>
         <p></p>
 
-        <h1>San Fransisco</h1>
+        <h1>{weatherData.city}</h1>
         <ul>
-          <li>Sunday 14.09, 10:00am</li>
-          <li> few clouds</li>
+          <li>{weatherData.date}</li>
+          <li className="text-capitalize">{weatherData.description}</li>
         </ul>
         <p></p>
 
         <div className="row">
           <div className="col-6">
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-              alt="Weather icon"
-            />
-            <span className="temperature">{temperature}</span>
+            <img src={weatherData.iconUrl} alt={weatherData.description} />
+
+            <span className="temperature">
+              {Math.round(weatherData.temperature)}
+            </span>
             <span className="unit">°C</span>
           </div>
 
           <div className="col-6">
             <ul>
               <li>precipitaion:15% </li>
-              <li>Humidity:85%,</li>
-              <li>Wind:2,06km/h</li>
+              <li>humidity:{weatherData.humidity}%</li>
+              <li>wind:{weatherData.wind} km/h</li>
             </ul>
           </div>
         </div>
@@ -68,9 +76,9 @@ export default function Weather() {
     );
   } else {
     const apiKey = "2d96d64425dca1d6eda00d942a281c0d";
-    let city = "San Fransisco";
 
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+
     axios.get(apiUrl).then(handleResponse);
 
     return "Loading...";
