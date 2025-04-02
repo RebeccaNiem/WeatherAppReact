80d29eeb0d0ad2dd043ea869d894e2d289b62b61
RebeccaNiem <161700704+RebeccaNiem@users.noreply.github.com>
2025-04-03 01:17:58 +0200

    changed city by default

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <div className="container"></div>
-      <Weather defaultCity="Moscow" />
+      <Weather defaultCity="New York" />
 
       <footer>
         This project was built by Rebecca and is hosted on{" "}
